00e7724d1202c8ecaf1850184984b5b1da992661
uraankhayayaal <uraankhayayaal@gmail.com>
2020-09-08 20:55:07 +0900

    list style for admin

--- RedactorFixAsset.php
+++ RedactorFixAsset.php
@@ -8,11 +8,14 @@ class RedactorFixAsset extends AssetBundle
 	public $sourcePath = '@vendor/uraankhayayaal/yii2-redactor/src/';
 
     public $css = [
+		'redactor.css',
+        'handle/handle.css',
     ];
     public $js = [
         'redactor.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
+        'backend\assets\AppAsset',
     ];
 }
